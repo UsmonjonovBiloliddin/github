9faaa74505c000e19da767c6a7d0b023c39d6e60
Nurzotbek0303 <nurzotbeke@gmail.com>
2025-08-14 19:13:15 +0500

    asdfghjkl

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Nurzotbek from "./nurzotbek";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -49,6 +50,7 @@ function App() {
       <h1>Assalomu alaykum</h1>
       <h1>Assalomu alaykum</h1>
       <h1>Assalomu alaykum</h1>
+      <Nurzotbek />
     </>
   );
 }
