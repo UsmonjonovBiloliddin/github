f6572513454ebbbee6ff71483ca63821b5081ebd
najmiddininomov <Inomovnajmiddin737@gmail.com>
2025-08-14 19:12:39 +0500

    dfvn fbaihrwc

---
+++ src/Najmiddin.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Najmiddin = () => {
+  return (
+    <div><button>hi</button>
+    
+    <button>dscmlsv mdfv</button>
+    </div>
+  )
+}
+
+export default Najmiddin
